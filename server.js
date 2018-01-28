e3a1a480da3b3112668e704e01c76ddc61fbf3ba
anaswardeh <wardeh@un.org>
2018-01-28 11:34:37 -0500

    firebase

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 const ArticleController = require("./controllers/ArticleController")
 const app = express();
 const PORT = process.env.PORT || 3001;
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,15 +25,6 @@ mongoose.connect(
     useMongoClient: true
   }
 );
-
-
-admin.initializeApp({
-  credential: process.env.FIREBASE_PRIVATE_KEY || admin.credential.cert(serviceAccount),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL || "firebase-adminsdk-xeiir@nytimes-e778c.iam.gserviceaccount.com",
-  databaseURL: "https://nytimes-e778c.firebaseio.com"
-});
-
-
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
